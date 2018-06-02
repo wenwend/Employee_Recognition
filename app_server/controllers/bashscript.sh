a09d17cdf72e54b76dd7502ac80d81201b85f10a
reynojon <reynojon@oregonstate.edu>
2018-06-01 21:42:26 -0400

    Update bashscript.sh

--- app_server/controllers/bashscript.sh
+++ app_server/controllers/bashscript.sh
@@ -10,6 +10,8 @@
 node ./app_server/controllers/createtex.js "$1" "$2" "$3" "$4" "$5"
 echo "current directory"
 ls
+echo "./app_server/controllers"
+ls ./app_server/controllers
 echo "root directory"
 ls ../../
 cat ./app_server/controllers/award.tex
