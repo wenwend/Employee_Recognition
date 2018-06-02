8a9b5e049615ff28bd1ed08cba3ccba2e29a049a
wenwend <wenwen@Anitas-MacBook.local>
2018-06-01 18:20:49 -0700

    Jon's update to show logs

--- app_server/controllers/bashscript.sh
+++ app_server/controllers/bashscript.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 # bashscript.sh awardType recipientName signatureURL presenterName awardDate recipientEmail
-# echo "launching script"
-# echo $1 #awardType    
-# echo $2 #recipientName
-# echo $3 #signatureURL
-# echo $4 #presenterName
-# echo $5 #awardDate
-# echo $6 #recipientEmail
+ echo "launching script"
+ echo $1 #awardType    
+ echo $2 #recipientName
+ echo $3 #signatureURL
+ echo $4 #presenterName
+ echo $5 #awardDate
+ echo $6 #recipientEmail
 node ./app_server/controllers/createtex.js "$1" "$2" "$3" "$4" "$5"
-#echo "current directory"
-#ls
-#echo "root directory"
-#ls ../../
-#cat ./app_server/controllers/award.tex
+echo "current directory"
+ls
+echo "root directory"
+ls ../../
+cat ./app_server/controllers/award.tex
 node ./app_server/controllers/converttex.js
-#ls
+ls
 node ./app_server/controllers/mailer.js "$6"
-#ls
+ls
 echo "script done"
