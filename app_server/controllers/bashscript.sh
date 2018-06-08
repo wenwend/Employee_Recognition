ce18e325b2b69c0ca95e4e66bb1e2ddc5a1dc16e
reynojon <reynojon@oregonstate.edu>
2018-06-07 20:41:20 -0400

    Add files via upload

--- app_server/controllers/bashscript.sh
+++ app_server/controllers/bashscript.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 # bashscript.sh awardType recipientName signatureURL presenterName awardDate recipientEmail
  echo "launching award creation script"
-# echo $1 #awardType    
-# echo $2 #recipientName
-echo $3 #signatureURL
-# echo $4 #presenterName
-# echo $5 #awardDate
-# echo $6 #recipientEmail
+
+#create image file from database
 node ./app_server/controllers/imageconverter.js $3
-node ./app_server/controllers/createtex.js "$1" "$2" "./app_server/controllers/signature.png" "$4" "$5"
-#echo "current directory"
-#ls
-#echo "./app_server/controllers"
-#ls ./app_server/controllers
-#echo "root directory"
-#ls ../../
-#cat ./app_server/controllers/award.tex
-pdflatex ./app_server/controllers/award.tex
-#node ./app_server/controllers/converttex.js
-#ls
+
+#convert image to .jpg if necessary
+SIGPNG="./app_server/controllers/signature.png"
+if [ -f $SIGPNG ]; then #Convert the png
+   echo "Converting signature to jpg"
+   node pngtojpg.js
+fi
+
+#create latex file from parameters
+#node ./app_server/controllers/createtex.js "$1" "$2" "./app_server/controllers/signature.png" "$4" "$5"
+
+#create latex file from parameters. If signature.jpg is not present, use default signature file
+SIGJPG="./app_server/controllers/signature.jpg"
+if [ -f $SIGJPG ]; then #Convert the png
+   node ./app_server/controllers/createtex.js "$1" "$2" "./app_server/controllers/signature.jpg" "$4" "$5"
+else
+   node ./app_server/controllers/createtex.js "$1" "$2" "./app_server/controllers/samplesignature.jpg" "$4" "$5"
+fi
+
+#convert latex file into a pdf
+node ./app_server/controllers/converttex.js
+
+#send pdf to recipientEmail
 node ./app_server/controllers/mailer.js "$6"
+
 echo "award creation script done"
---
+++ app_server/controllers/pngtojpg.js
@@ -0,0 +1,8 @@
+//SOURCE: https://www.npmjs.com/package/png-to-jpeg
+//REQUIRES: node package: png-to-jpeg@1.0.1
+const fs = require("fs");
+const pngToJpeg = require('png-to-jpeg');
+ 
+let buffer = fs.readFileSync("./signature.png");
+pngToJpeg({quality: 90})(buffer)
+.then(output => fs.writeFileSync("./signature.jpg", output));
