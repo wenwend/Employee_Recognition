44d761f30b4320f475ec0e78f06d0e4562b6d939
reynojon <reynojon@oregonstate.edu>
2018-06-07 21:23:54 -0400

    Update bashscript.sh

--- app_server/controllers/bashscript.sh
+++ app_server/controllers/bashscript.sh
@@ -15,16 +15,25 @@ fi
 #create latex file from parameters
 #node ./app_server/controllers/createtex.js "$1" "$2" "./app_server/controllers/signature.png" "$4" "$5"
 
+#echo "current directory"
+#ls
+#echo "./app_server/controllers"
+#ls ./app_server/controllers
+#echo "root directory"
+#ls ../../
+
 #create latex file from parameters. If signature.jpg is not present, use default signature file
 SIGJPG="./app_server/controllers/signature.jpg"
 if [ -f $SIGJPG ]; then #Convert the png
+   echo "Using signature.jpg"
    node ./app_server/controllers/createtex.js "$1" "$2" "./app_server/controllers/signature.jpg" "$4" "$5"
 else
+   echo "using samplesignature.jpg"
    node ./app_server/controllers/createtex.js "$1" "$2" "./app_server/controllers/samplesignature.jpg" "$4" "$5"
 fi
 
 #convert latex file into a pdf
-node ./app_server/controllers/converttex.js
+pdflatex ./app_server/controllers/award.tex
 
 #send pdf to recipientEmail
 node ./app_server/controllers/mailer.js "$6"
